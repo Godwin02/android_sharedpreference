00905efbb5b8904667095529ec2da926370da7ab
Godwin02 <godwinbmenachery2024a@mca.ajce.in>
2023-12-13 13:00:42 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sharedpreference/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.sharedpreference;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText username,password;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        username=findViewById(R.id.username);
+        password=findViewById(R.id.password);
+    }
+    public void save(View view) {
+        String email=username.getText().toString();
+        String pass=password.getText().toString();
+
+        SharedPreferences preferences=getSharedPreferences("email",MODE_PRIVATE);
+        SharedPreferences.Editor editor=preferences.edit();
+        editor.putString("email",email);
+        editor.putString("password",pass);
+        editor.commit();
+        Toast.makeText(MainActivity.this,"Shared preferences saved successfully",Toast.LENGTH_LONG).show();
+        Intent intent=new Intent(this,MainActivity.class);
+        startActivity(intent);
+    }
+}
